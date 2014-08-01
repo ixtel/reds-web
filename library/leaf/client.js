1572ab95315e7ee18e8d99a0da74633330424474
letorbi <torben.letorbi@gmail.com>
2014-08-01 22:15:02 +0200

    Signin and signup preparation calls and credentials database object added to Leaf client

--- library/leaf/client.js
+++ library/leaf/client.js
@@ -1,34 +1,82 @@
 (function(){
-"use strict"; 
+"use strict";
 
-var Leaf = function(url, cryptoFacility) {
-	this.url = url;
-	this.crypto = cryptoFacility;
+// INFO Credential database
+
+var Credentials = new Object();
+
+Credentials.registerLeafClient = function() {
+	var id = Math.floor(Math.random()*0xffffffff);
+	while (Credentials[id])
+		id = Math.floor(Math.random()*0xffffffff);
+	Credentials[id] = new Object();
+	return id;
 }
 
-Leaf.prototype.signup = function(name, password, callback) {
+Credentials.unregisterLeafClient = function(id) {
+	delete Credentials[id];
+}
+
+// INFO LeafClient client module
+
+var LeafClient = function(url, cryptoFacility) {
+	this.url = url;
+	this.crypto = cryptoFacility;
+	this.id = Credentials.registerLeafClient();
+	console.log(this.id);
+}
+
+LeafClient.prototype.sendJSON = function(method, path, data, callback) {
 	var xhr = new XMLHttpRequest();
 	xhr.addEventListener("load", onLoad, false);
-	xhr.open("POST", this.url+"/user/foobar", true);
-	xhr.send("user data");
+	xhr.open(method, this.url+path, true);
+	xhr.setRequestHeader("Content-Type", "application/json;charset=encoding");
+	xhr.send(data ? JSON.stringify(data) : undefined);
 
 	function onLoad() {
 		callback(xhr.responseText);
 	}
 }
 
-Leaf.prototype.signin = function(name, password, callback) {
-	var xhr = new XMLHttpRequest();
-	xhr.addEventListener("load", onLoad, false);
-	xhr.open("GET", this.url+"/user/foobar", true);
-	xhr.send();
+LeafClient.prototype.signup = function(name, password, callback) {
+	var alias = this.crypto.generateSecureHash(name, password);
+	var salt = this.crypto.generateKey();
+	var ksalt = this.crypto.generateKey();
+	var ssalt = this.crypto.generateKey();
+	var namepw = this.crypto.concatenateStrings(name, password);
+	var seed = this.crypto.generateSecureHash(namepw, salt);
+	var authKeypair = this.crypto.generateKeypair(seed);
+	this.sendJSON("POST", "/user", {
+		'alias': alias,
+		'salt': salt,
+		'ksalt': ksalt,
+		'ssalt': ssalt,
+		'auth': authKeypair.publicKey
+	}, afterSend.bind(this));
 
-	function onLoad() {
-		callback(xhr.responseText);
+	function afterSend(data) {
+		Credentials[this.id].alias = alias;
+		console.log(Credentials);
+	}
+}
+
+LeafClient.prototype.signin = function(name, password, callback) {
+	var alias = this.crypto.generateSecureHash(name, password);
+	var salt = this.crypto.generateKey();
+	var ksalt = this.crypto.generateKey();
+	var ssalt = this.crypto.generateKey();
+	var namepw = this.crypto.concatenateStrings(name, password);
+	var seed = this.crypto.generateSecureHash(namepw, salt);
+	var authKeypair = this.crypto.generateKeypair(seed);
+	this.sendJSON("GET", "/user/"+alias, null, afterSend.bind(this));
+
+	function afterSend(data) {
+		Credentials[this.id].alias = alias;
+		console.log(Credentials);
 	}
 }
 
 // NOTE Export when loaded as a CommonJS module, add to global reds object otherwise.
-typeof exports=='object' ? exports=Leaf : (self.reds=self.reds||new Object()).Leaf=Leaf;
+typeof exports=='object' ? exports=LeafClient : (self.reds=self.reds||new Object()).LeafClient=LeafClient;
 
 })();
