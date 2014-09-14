94342282ed8ded0b9013ca575ebbc6996fe99d19
letorbi <torben.letorbi@gmail.com>
2014-09-14 11:03:27 +0200

    Remove mySession from example node

--- example/node/main.js
+++ example/node/main.js
@@ -1,22 +1,5 @@
 var NodeServer = require("../../library/node/Server");
-var NodeSession = require("../../library/node/Session");
 var config = require("./config.json");
 
-var MySession = function(config, request, response) {
-	NodeSession.call(this, config, request, response);
-
-	console.log("MySession");
-	for (var prop in MySession.prototype.StorageFacilities)
-		console.log(prop);
-	console.log("NodeSession");
-	for (var prop in NodeSession.prototype.StorageFacilities)
-		console.log(prop);
-}
-
-MySession.prototype = Object.create(NodeSession.prototype);
-
-//MySession.prototype.registerStorageFacility(require("../../library/shared/crypto/cryptojs"));
-//NodeSession.prototype.registerStorageFacility(require("../../library/shared/crypto/sjcl"));
-
-var server = new NodeServer(config, MySession);
+var server = new NodeServer(config);
 server.run();
