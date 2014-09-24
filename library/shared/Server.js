e5f39ef03b7fae03ea3b7581616e0e2802ae32b4
letorbi <torben.letorbi@gmail.com>
2014-09-24 16:20:04 +0200

    Remove uncaughtException handling from server

--- library/shared/Server.js
+++ library/shared/Server.js
@@ -19,33 +19,12 @@ exports.prototype.run = function() {
 }
 
 exports.prototype.setup = function() {
-	// NOTE Restart the worker if it crashed
-	// TODO Enable only for production
-	cluster.addListener("exit", function(worker, code, signal) {
-		console.info(worker.process.pid +" EXIT "+code+" "+signal);
-		setTimeout(function(){cluster.fork()}, 5000);
-	});
-	
 	for (var i = 0; i < this.config.workers; i++)
 		cluster.fork();
 }
 
 exports.prototype.connect = function() {
 	console.info(process.pid +" CONNECT "+this.config.host+" "+this.config.port);
-
-	// NOTE Exit the worker instead of crashing node on exceptions
-	// TODO Enable only for production
-	process.addListener("uncaughtException", function(error) {
-		try {
-			console.error(process.pid +" EXCEPTION "+error);
-			this.httpd.close();
-			process.exit(1);
-		}
-		catch (e) {
-			console.log("FATAL "+e);
-			process.exit(2);
-		}	
-	}.bind(this));
 
 	this.httpd = http.createServer();
 	this.httpd.listen(this.config.port, this.config.host);
@@ -54,8 +33,6 @@ exports.prototype.connect = function() {
 
 exports.prototype.listen = function(request, response) {
 	console.info(process.pid +" LISTEN "+request.method+" "+request.url); // DEBUG
-	response.end();
-	return;
 	var session = new this.Session(this.config, request, response);
 	session.run();
 }
