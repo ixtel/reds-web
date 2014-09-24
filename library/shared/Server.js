65c9c9e86ec69ffa1bac10072a1762f5e51e3111
letorbi <torben.letorbi@gmail.com>
2014-09-24 09:31:05 +0200

    Remove domain stuff from server

--- library/shared/Server.js
+++ library/shared/Server.js
@@ -1,35 +1,10 @@
 var cluster = require("cluster");
 var http = require("http");
 var os = require("os");
-var domain = require("domain");
-
-global.listDomains = function() {
-	var adom = domain.active;
-	console.log("domain stack [");
-	for (var i=0; i<domain._stack.length; i++) {
-		console.log("index="+i+" equals_active="+(domain._stack[i] == adom));
-		console.log(require("util").inspect(domain._stack[i], { showHidden: true, depth: 1 }))
-	}
-	console.log("]");
-}
-
-global.cleanDomainLeaks = function(note) {
-	var counter = -1;
-	var adom = domain.active;
-	if (adom) {
-		while (domain.active==adom) {
-			counter++;
-			domain.active.exit();
-		}
-		console.log("DEBUG '"+note+"' domain leaks: "+counter);
-		adom.enter();
-	}
-}
-
 
 module.exports = exports = function(config, Session) {
-	this.hooks = new Object();
 	this.config = config;
+	this.httpd = null;
 	this.Session = Session;
 
 	if (this.config.workers == "cores")
@@ -44,17 +19,11 @@ exports.prototype.run = function() {
 }
 
 exports.prototype.setup = function() {
+	// NOTE Restart the worker if it crashed
+	// TODO Enable only for production
 	cluster.addListener("exit", function(worker, code, signal) {
 		console.info(worker.process.pid +" EXIT "+code+" "+signal);
-	});
-
-	cluster.addListener("disconnect", function(worker) {
-		// NOTE Since ondisconnect is triggerd by worker.disconnect() and
-		//      process.exit(), we only fork a new worker if the old worker
-		//      killed itself via diconnect(). Otherwise we assume an
-		//      unresolvable error and don't respawn. 
-		if (worker.suicide)
-			cluster.fork();
+		setTimeout(function(){cluster.fork()}, 5000);
 	});
 	
 	for (var i = 0; i < this.config.workers; i++)
@@ -63,38 +32,30 @@ exports.prototype.setup = function() {
 
 exports.prototype.connect = function() {
 	console.info(process.pid +" CONNECT "+this.config.host+" "+this.config.port);
-	global.httpd = http.createServer();
-	global.httpd.listen(this.config.port, this.config.host, function(error) {
-		if (error)
-			this.fatal(error);
-	 	global.httpd.addListener("error", this.disconnect.bind(this));
-	 	global.httpd.addListener("request", this.listen.bind(this));
+
+	// NOTE Exit the worker instead of crashing node on exceptions
+	// TODO Enable only for production
+	process.addListener("uncaughtException", function(error) {
+		try {
+			console.error(process.pid +" EXCEPTION "+error);
+			this.httpd.close();
+			process.exit(1);
+		}
+		catch (e) {
+			console.log("FATAL "+e);
+			process.exit(2);
+		}	
 	}.bind(this));
+
+	this.httpd = http.createServer();
+	this.httpd.listen(this.config.port, this.config.host);
+	this.httpd.addListener("request", this.listen.bind(this));
 }
 
 exports.prototype.listen = function(request, response) {
 	console.info(process.pid +" LISTEN "+request.method+" "+request.url); // DEBUG
+	response.end();
+	return;
 	var session = new this.Session(this.config, request, response);
-	//session.addListener("error", function(){response.end();});
-	session.addListener("error", this.disconnect.bind(this));
 	session.run();
-}
-
-exports.prototype.disconnect = function(error) {
-	try {
-		console.warn(process.pid +" DISCONNECT");
-		console.log(error); // DEBUG
-		setTimeout(function(){process.exit(2);}, 5000).unref();
-		global.httpd.close();
-		cluster.worker.disconnect();
-	}
-	catch (e) {
-		this.fatal(e);
-	}
-}
-
-exports.prototype.fatal = function(error) {
-	console.error(process.pid +" FATAL");
-	console.log(error); // DEBUG
-	process.exit(1);
 }
