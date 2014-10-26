0dfdbf6ce9d2d9687541ba2e8a436f845585652a
letorbi <torben.letorbi@gmail.com>
2014-10-26 17:20:13 +0100

    Remove trash requirement from trigger functions for NodePg storage facility

--- library/shared/storage/NodePg.sql
+++ library/shared/storage/NodePg.sql
@@ -5,58 +5,96 @@ CREATE OR REPLACE FUNCTION plperl_inline_handler(internal) RETURNS void AS '$lib
 CREATE OR REPLACE FUNCTION plperl_validator(oid) RETURNS void AS '$libdir/plperl' LANGUAGE C STRICT;
 CREATE OR REPLACE TRUSTED PROCEDURAL LANGUAGE plperl HANDLER plperl_call_handler INLINE plperl_inline_handler VALIDATOR plperl_validator;
 
--- INFO Trash relation and delete relation child
+-- INFO Simulation trigger
+-- NOTE Use only for DELETE FROM entities WHERE ... !
 
-CREATE OR REPLACE FUNCTION is_trashed_relation(integer, integer) RETURNS boolean AS $$
-	return (index($_SHARED{trashed_relations}, ':'.$_[0].':'.$_[1].':')!=-1 ? true : false);
+CREATE OR REPLACE FUNCTION simulate(text) RETURNS text AS $$
+	$_SHARED{simulation_active} = 1;
+	spi_exec_query($_[0], 0);
+	$_SHARED{simulation_active} = 0;
+	chop($_SHARED{simulation_result});
+	return $_SHARED{simulation_result};
 $$ LANGUAGE plperl;
 
+CREATE OR REPLACE FUNCTION set_cascade_domain(integer) RETURNS text AS $$
+	$_SHARED{cascade_domain} = $_[0];
+	return $_SHARED{cascade_domain};
+$$ LANGUAGE plperl;
 
-CREATE OR REPLACE FUNCTION trash_relation_and_delete_child() RETURNS trigger AS $$
-	spi_exec_query("INSERT INTO relations_trash (parent,child) VALUES ($_TD->{old}{parent},$_TD->{old}{child});", 1);
-	$_SHARED{trashed_relations} = $_SHARED{trashed_relations}.':'.$_TD->{old}{parent}.':'.$_TD->{old}{child}.':';
-	spi_exec_query("DELETE FROM entities WHERE entities.eid = $_TD->{old}{child};", 1);
+-- INFO Entity triggers
+
+CREATE OR REPLACE FUNCTION before_delete_entity() RETURNS TRIGGER AS $$
+	if ($_SHARED{cascade_domain} && ($_SHARED{cascade_domain} != $_TD->{old}{did})) {
+		return "SKIP";
+	}
+	if ($_SHARED{simulation_active}) {
+		$_SHARED{simulation_result} = $_SHARED{simulation_result}.$_TD->{old}{eid}.",";
+		spi_exec_query("DELETE FROM relations WHERE parent = $_TD->{old}{eid}", 1);
+		return "SKIP";
+	}
 	return;
 $$ LANGUAGE plperl;
+DROP TRIGGER IF EXISTS before_delete_entity_trigger ON entities;
+CREATE TRIGGER before_delete_entity_trigger BEFORE DELETE ON entities
+  FOR EACH ROW EXECUTE PROCEDURE before_delete_entity();
 
-DROP TRIGGER IF EXISTS trash_relation_and_delete_child_trigger ON relations;
-CREATE TRIGGER trash_relation_and_delete_child_trigger AFTER DELETE ON relations
-  FOR EACH ROW EXECUTE PROCEDURE trash_relation_and_delete_child();
-
--- INFO Trash entity
-
-CREATE OR REPLACE FUNCTION is_trashed_entity(integer) RETURNS boolean AS $$
-	return (index($_SHARED{trashed_entities}, ':'.$_[0].':')!=-1 ? true : false);
-$$ LANGUAGE plperl;
-
-CREATE OR REPLACE FUNCTION trash_entity() RETURNS TRIGGER AS $$
-	spi_exec_query("INSERT INTO entities_trash (eid,tid,did) VALUES ($_TD->{old}{eid},$_TD->{old}{tid},$_TD->{old}{did});", 1);
-	$_SHARED{trashed_entities} = $_SHARED{trashed_entities}.':'.$_TD->{old}{eid}.':';
+CREATE OR REPLACE FUNCTION after_delete_entity() RETURNS TRIGGER AS $$
+	spi_exec_query("DELETE FROM relations WHERE parent = $_TD->{old}{eid}", 1);
 	return;
 $$ LANGUAGE plperl;
+DROP TRIGGER IF EXISTS after_delete_entity_trigger ON entities;
+CREATE TRIGGER after_delete_entity_trigger AFTER DELETE ON entities
+  FOR EACH ROW EXECUTE PROCEDURE after_delete_entity();
 
-DROP TRIGGER IF EXISTS trash_entity_trigger ON entities;
-CREATE TRIGGER trash_entity_trigger AFTER DELETE ON entities
-  FOR EACH ROW EXECUTE PROCEDURE trash_entity();
+-- INFO Relation triggers
+	
+CREATE OR REPLACE FUNCTION before_delete_relation() RETURNS TRIGGER AS $$
+	if ($_SHARED{simulation_active}) {
+		spi_exec_query("DELETE FROM entities WHERE eid = $_TD->{old}{child}", 1);
+		return "SKIP";
+	}
+	return;
+$$ LANGUAGE plperl;
+DROP TRIGGER IF EXISTS before_delete_relation_trigger ON relations;
+CREATE TRIGGER before_delete_relation_trigger BEFORE DELETE ON relations
+  FOR EACH ROW EXECUTE PROCEDURE before_delete_relation();
 
--- INFO testing
+CREATE OR REPLACE FUNCTION after_delete_relation() RETURNS TRIGGER AS $$
+	spi_exec_query("DELETE FROM entities WHERE eid = $_TD->{old}{child}", 1);
+	return;
+$$ LANGUAGE plperl;
+DROP TRIGGER IF EXISTS after_delete_relation_trigger ON relations;
+CREATE TRIGGER after_delete_relation_trigger AFTER DELETE ON relations
+  FOR EACH ROW EXECUTE PROCEDURE after_delete_relation();
 
--- NOTE Start the test
+-- INFO Testing
+
+INSERT INTO domains (did,pid) VALUES (1,1);
+INSERT INTO domains (did,pid) VALUES (2,1);
 INSERT INTO entities (eid,tid,did) VALUES (1,1,1);
 INSERT INTO entities (eid,tid,did) VALUES (2,1,1);
 INSERT INTO entities (eid,tid,did) VALUES (3,1,1);
+INSERT INTO entities (eid,tid,did) VALUES (4,1,1);
+INSERT INTO entities (eid,tid,did) VALUES (5,1,2);
+INSERT INTO relations (parent,child) VALUES (1,2);
+INSERT INTO relations (parent,child) VALUES (1,5);
 INSERT INTO relations (parent,child) VALUES (2,3);
+INSERT INTO relations (parent,child) VALUES (4,3);
+SELECT * FROM domains;
+SELECT * FROM entities;
+SELECT * FROM relations;
+
+SELECT set_cascade_domain(1);
+SELECT simulate('DELETE FROM entities WHERE eid=1');
+SELECT * FROM domains;
+SELECT * FROM entities;
+SELECT * FROM relations;
+
 DELETE FROM entities WHERE eid=1;
-DELETE FROM entities WHERE eid=2;
-DELETE FROM entities WHERE eid=3;
--- NOTE Check results
-SELECT is_trashed_entity(0);
-SELECT is_trashed_entity(1);
-SELECT is_trashed_entity(2);
-SELECT is_trashed_entity(3);
-SELECT is_trashed_entity(4);
-SELECT is_trashed_relation(2,3);
-SELECT is_trashed_relation(2,4);
--- NOTE Clean up stuff
-TRUNCATE entities_trash RESTART IDENTITY;
-TRUNCATE relations_trash;
+SELECT * FROM domains;
+SELECT * FROM entities;
+SELECT * FROM relations;
+
+TRUNCATE domains RESTART IDENTITY CASCADE;
+TRUNCATE entities RESTART IDENTITY CASCADE;
+TRUNCATE relations;
