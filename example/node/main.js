b13c6685a948a3cc97c3ccd39d3f0fb04834dc09
Torben <torben.letorbi@gmail.com>
2014-12-08 07:07:18 +0100

    Remove old example directory

--- example/node/main.js
+++
@@ -1,5 +0,0 @@
-var NodeServer = require("../../node/Server");
-var config = require("./config.json");
-
-var server = new NodeServer(config);
-server.run();
--- example/pod/main.js
+++
@@ -1,5 +0,0 @@
-var PodServer = require("../../pod/Server.js");
-var config = require("./config.json");
-
-var server = new PodServer(config);
-server.run();
