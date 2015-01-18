ixtel/reds-web

commit c63f02dfbc4849d012fd5912480f94e91e08a6f3

Fix: Pod crashes when node authorization is missing

pod/Session.js
@@ -39,7 +39,7 @@ exports.prototype.authorizeNode = function(callback) {
     // TODO Ignoring a missing authorization and simply returning the
     //      request data is probably not the best idea.
     if (!this.authorization)
-        return callback(null, this.requestJson);
+        return callback(new HttpError(400, "Missing authorization"));
     // NOTE Note this check won't be needed once the session can handle multiple facilities
     if (this.authorization['crypto'] != this.crypto.name)
         return callback(new HttpError(400, "Unsupported crypto facility"));

commit d4bcb5579e39d251dbeb2af2243ac625f0ed4346

Fix: Unable to register new node on pod (regression from c63f02d)

pod/Session.js
@@ -25,7 +25,10 @@ exports.prototype.HookHandlers = {
 }
 
 exports.prototype.delegate = function() {
-    this.authorizeNode(afterAuthorizeNode.bind(this));
+    if (this.request.method == "POST" && this.selector.hook == "/!/node")
+        Session.prototype.delegate.call(this);
+    else
+        this.authorizeNode(afterAuthorizeNode.bind(this));
 
     function afterAuthorizeNode(error, result) {
         if (error)